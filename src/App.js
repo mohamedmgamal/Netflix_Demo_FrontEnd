d243a45f515fee2db20b3ce08a8283959e422eda
mohamedmgamal <mohamedmgamal_97@yahoo.com>
2021-03-27 19:19:53 +0200

    smaall edit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-} from "react-router-dom";
-import { Redirect } from 'react-router'
-import './staticUrls'
-import {urls} from "./staticUrls";
 import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return <h1>Hello World</h1>
