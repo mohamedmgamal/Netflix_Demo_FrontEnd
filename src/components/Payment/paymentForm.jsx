c98cf2b5b000126da6ad0e130022f174d24e0967
Mohamed Gamal <mohamedmgamal_97@yahoo.com>
2021-03-31 19:34:47 +0200

    small

--- src/components/Payment/paymentForm.jsx
+++ src/components/Payment/paymentForm.jsx
@@ -19,9 +19,8 @@ class PaymentForm extends Component {
                             <input type="text" placeholder="First Name" className="form-control" />                                        
                             <input type="text" placeholder="Last Name" className="form-control" />
                             <input type="text" placeholder="Card Number" className="form-control" /> 
-                            <input type="text" placeholder="Expiration Date (MM/YY)" className="form-control" /> 
-                            <input type="text" placeholder="Security Code (CVV)" maxLength="50" className="form-control" id="cvv" /> 
-                            <i class="fa fa-question-circle fa-2x" style={{marginBottom:"2px"}}></i>
+                            <input type="date" placeholder="Expiration Date (MM/YY)" className="form-control" />
+                            <input type="text" placeholder="Security Code (CVV)" maxLength="50" className="form-control" id="cvv" />
                         </div>
                         <div className="premium" style={{backgroundColor:"#DF3B45",paddingTop:"2.5%",paddingLeft:"1%"}}>
                             <span style={{color:"white",fontWeight:"bold"}}>Premium Plan</span>
@@ -32,7 +31,10 @@ class PaymentForm extends Component {
                         and that you are over 18. Netflix will automatically continue your membership
                         and charge the monthly membership free (currently EGP200) to your payment method until you cancel.
                         </p>
-                    </div>       
+                        <div className="premium" style={{backgroundColor:"#DF3B45",paddingTop:"2.5%",paddingLeft:"1%",textAlign:"center"}}>
+                            <span style={{color:"white",fontWeight:"bold"}}>Continue</span>
+                        </div>
+                    </div>
                 </div>
             </React.Fragment>
          );
