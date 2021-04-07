92ee8941469254836998cfaaf81b82337118bde7
omaralkasaby <omaralkasaby899@gmail.com>
2021-04-07 14:14:19 +0200

    Fetch data and last updating

--- src/components/Home/requests.js
+++ src/components/Home/requests.js
@@ -1,13 +1,15 @@
-const API_KEY="423cddd404ed1d0527c4df22b37dc6e6";
+// const API_KEY="423cddd404ed1d0527c4df22b37dc6e6";
+// axios.get(webApiUrl);
 
 const requests={
-    fetchTrending:`/trending/all/week?api_key=${API_KEY}&language=en-US`,
-    fetchNetflixOriginals:`/discover/tv?api_key=${API_KEY}&with_network=213`,
-    fetchTopRated:`/movie/top_rated?api_key=${API_KEY}&language=en-US`,
-    fetchActionMovies:`discover/movie?api_key=${API_KEY}&with_genres=28`,
-    fetchComedyMovies:`discover/movie?api_key=${API_KEY}&with_genres=35`,
-    fetchHorrorMovies:`discover/movie?api_key=${API_KEY}&with_genres=27`,
-    fetchRomanceMovies:`discover/movie?api_key=${API_KEY}&with_genres=10749`,
-    fetchDocumentaries:`discover/movie?api_key=${API_KEY}&with_genres=99`
+    fetchTrending:`/Trending`,
+    fetchNetflixOriginals:`/Netflix Originals`,
+    fetchTopRated:`/Top Rated`,
+    fetchActionMovies:`/Action`,
+    fetchComedyMovies:`/Comedy`,
+    fetchHorrorMovies:`/Horror`,
+    fetchRomanceMovies:`/Romantic`,
+    fetchDocumentaries:`/Documentaries`,
+    fetchBanner:`/Banner`
 }
 export default requests;
